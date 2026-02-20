fb89a4ca760593afb351663399eeba91c9afb1f7
Pete Lowry <lowrysoftdev@gmail.com>
2026-02-20 21:44:43 +0000

    Include some examples for YOUTUBE_URL to use in testing

--- settings.py
+++ settings.py
@@ -1,1 +1,7 @@
 YOUTUBE_URL = "https://www.youtube.com/watch?v=VpAegQyncmQ"
+
+# Good examples to test out functionality
+# - https://www.youtube.com/watch?v=VpAegQyncmQ  contains farsi
+# - https://www.youtube.com/watch?v=eaAe9GcpLJI  contains solo not in DB and video with some music in
+# - https://www.youtube.com/watch?v=DW37U-yvd60  first song in first block not showing lyrics
+# - https://www.youtube.com/watch?v=8ThEhWB-KtQ  block of four songs
