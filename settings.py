d3b284803316949eebdf6ec7b19fe76a37eda442
Pete Lowry <lowrysoftdev@gmail.com>
2026-02-24 23:46:58 +0000

    Add comment to clarify where YOUTUBE_URL is used

--- settings.py
+++ settings.py
@@ -1,7 +1,8 @@
-YOUTUBE_URL = "https://www.youtube.com/watch?v=VpAegQyncmQ"
+# YOUTUBE_URL used in notebooks 03 and 04
+YOUTUBE_URL = "https://www.youtube.com/watch?v=WosCA-fzPXs"
 
 # Good examples to test out functionality
 # - https://www.youtube.com/watch?v=VpAegQyncmQ  contains farsi
-# - https://www.youtube.com/watch?v=eaAe9GcpLJI  contains solo not in DB and video with some music in
-# - https://www.youtube.com/watch?v=DW37U-yvd60  first song in first block not showing lyrics
+# - https://www.youtube.com/watch?v=eaAe9GcpLJI  contains solo and video with bg music
+# - https://www.youtube.com/watch?v=DW37U-yvd60  first song not showing lyrics
 # - https://www.youtube.com/watch?v=8ThEhWB-KtQ  block of four songs
