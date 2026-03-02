bf6bd8b64d4e0871b5060ed92bc48d6cdcc5bf16
Pete Lowry <lowrysoftdev@gmail.com>
2026-03-02 13:58:52 +0000

    Update YOUTUBE_URL for testing v2 of model

--- settings.py
+++ settings.py
@@ -1,8 +1,10 @@
 # YOUTUBE_URL used in notebooks 03 and 04
-YOUTUBE_URL = "https://www.youtube.com/watch?v=WosCA-fzPXs"
+YOUTUBE_URL = "https://www.youtube.com/watch?v=jUgvQHJzcho"
 
 # Good examples to test out functionality
 # - https://www.youtube.com/watch?v=VpAegQyncmQ  contains farsi
 # - https://www.youtube.com/watch?v=eaAe9GcpLJI  contains solo and video with bg music
 # - https://www.youtube.com/watch?v=DW37U-yvd60  first song not showing lyrics
 # - https://www.youtube.com/watch?v=8ThEhWB-KtQ  block of four songs
+# - https://www.youtube.com/watch?v=t4x2gUGEYI4  v1 model struggled
+# - https://www.youtube.com/watch?v=jUgvQHJzcho  v1 model struggled
